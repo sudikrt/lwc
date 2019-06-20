f8cfaeeefeb093795348d2e785e6a80681377b2f
sudikrt <sudarshankaranth18@gmail.com>
2019-06-20 08:35:17 +0530

    Standard Object page navigation

---
+++ navToFilesHome/navToFilesHome.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class NavToFilesHome extends NavigationMixin (LightningElement){
+    handleClick () {
+        this [NavigationMixin.Navigate] ({
+            type : 'standard__objectPage',
+            attributes : {
+                objectApiName : 'ContentDocument',
+                actionName : 'home'
+            }
+        })
+    }
+}
