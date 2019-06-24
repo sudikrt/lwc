7c47845f5d9b15ebf597b0aab1e9e843ddfad805
sudikrt <sudarshankaranth18@gmail.com>
2019-06-24 23:55:06 +0530

    wireGetObjectInfo

---
+++ wireGetObjectInfo/wireGetObjectInfo.js
@@ -0,0 +1,18 @@
+import { LightningElement, wire } from 'lwc';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+
+export default class WireGetObjectInfo extends LightningElement {
+    objectApiName;
+    @wire(getObjectInfo, {objectApiName : '$objectApiName'}) objectInfo;
+
+    handleClick () {
+        this.objectApiName = this.template.querySelector ('lightning-input').value;
+    }
+
+    get objectInfoStr() {
+        return this.objectInfo 
+        ?  JSON.stringify(this.objectInfo.data, null, 2)
+        : ''; 
+    }
+
+}
