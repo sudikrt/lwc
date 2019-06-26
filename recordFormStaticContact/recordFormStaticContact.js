8fcb1b5fa2690a5327de6ad011124767dcc4c270
sudikrt <sudarshankaranth18@gmail.com>
2019-06-26 23:51:59 +0530

    recordFormStaticContact

---
+++ recordFormStaticContact/recordFormStaticContact.js
@@ -0,0 +1,16 @@
+import { LightningElement, api } from 'lwc';
+
+import ACCOUNT_FIELD from '@salesforce/schema/Contact.AccountId';
+import NAME_FIELD from '@salesforce/schema/Contact.Name';
+import TITLE_FIELD from '@salesforce/schema/Contact.Title';
+import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+
+
+export default class RecordFormStaticContact extends LightningElement {
+
+    @api recordId;
+    @api objectApiName;
+
+    fields = [ACCOUNT_FIELD, NAME_FIELD, TITLE_FIELD, PHONE_FIELD, EMAIL_FIELD];
+}
