c1af0ac74f9a25f7ffd73645135e56c5c346fa15
sudikrt <sudarshankaranth18@gmail.com>
2019-07-03 08:01:28 +0530

    miscContentAsset

---
+++ miscContentAsset/miscContentAsset.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import recpLogo from '@salesforce/contentAssetUrl/X220pxZeiss_logosvg';
+
+export default class MiscContentAsset extends LightningElement {
+    imgURL = recpLogo;
+}
